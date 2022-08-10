4a137ff664ef0d7e3c8c453de5eca8580d8087e8
John Doe <john.doe@example.com>
2022-08-10 18:07:48 -0400

    add indices to capture. whew .

--- capture.py
+++ capture.py
@@ -8,6 +8,23 @@ from ar import Peer, Wallet, DataItem
 from ar.utils import create_tag
 from bundlr import Node
 
+# indexes a balanced tree of past indices
+class append_indices(list):
+    def __init__(self, degree = 2, initial_indices = []):
+        super().__init__(*initial_indices)
+        self.degree = degree
+        self.leaf_count = 0
+    def append(self, last_indices_id):
+        self.leaf_count += 1
+        leaf_count = self.leaf_count
+        idx = 0
+        for idx, (sub_leaf_count, value) in enumerate(self):
+            if sub_leaf_count * self.degree <= leaf_count:
+                break
+            leaf_count -= sub_leaf_count
+            idx += 1 # to append if the loop falls through
+        self[idx:] = [(leaf_count, last_indices_id)]
+
 try:
     wallet = Wallet('identity.json')
 except:
@@ -33,6 +50,8 @@ start_block = None
 prev = None
 peer = Peer()
 offset = 0
+indices = append_indices(3)
+
 while True:
     raw = capture.read(100000)
     data = send(raw)
@@ -45,6 +64,7 @@ while True:
         current_block = current_block,
         api_block = data['block'],
         start_block = start_block
+        index = [value for leaf_count, value in indices]
     )
     result = send(json.dumps(metadata).encode())
     offset += len(raw)
@@ -52,6 +72,7 @@ while True:
     if first is None:
         first = prev
         start_block = current_block
+    indices.append(dict(dataitem=prev, current_block=current_block, offset=offset))
 
     #eta = current_block['timestamp'] + (result['block'] - current_block['height']) * 60 * 2
     #eta = datetime.fromtimestamp(eta)
