27bb5e7ab47347f12979a56344e413814ada5d67
John Doe <john.doe@example.com>
2022-08-10 11:32:44 -0400

    wip

--- download.py
+++ download.py
@@ -9,14 +9,80 @@ class Stream:
     def __init__(self, metadata, peer):
         self.peer = peer
         self.start_block = self.block(metadata['start_block'])
-        self.block = self.start_block
+        self.last_block = None
         self.offset = 0
         self.first = metadata['first']
-    def _scan_block(self):
-        for tx in self.block.
-    def next(self):
-        self.block.height
-        self.block.trans
+        self.bundle_by_item = {}
+        self.data_by_offset = {}
+    def _scan_block(self, block):
+        if type(block) in (int, str):
+            block = self.block(block)
+        for txid in block.txs:
+            tx = Transaction.frombytes(peer.tx2(txid))
+            tags = tx.tags
+            if any((tag['name'].startswith(b'Bundle') for tag in tags)):
+                try:
+                    stream = self.peer.stream(txid)
+                except ArweaveException as exc:
+                    print(exc)
+                    continue
+                with stream:
+                    header = ANS104Header.from_tags_stream(tags, stream)
+                    offset = header.get_len_bytes()
+                    for id, length in header.length_by_id.items():
+                        def head_fetcher():
+                            stream.seek(offset)
+                            return ANS104DataItemHeader.from_tags_stream(tags, stream)
+                        def full_fetcher():
+                            stream.seek(offset)
+                            return DataItem.from_tags_stream(tags, stream)
+                        yield (txid, id, length, head_fetcher, full_fetcher)
+                        offset += length
+    def iterate_blocks(self, start_block, max_height):
+        if type(start_block) in (int, str):
+            start_block = self.block(start_block)
+        yield start_block
+        for height in range(start_block.height, max_height + 1):
+            yield aelf.block(height)
+    def iterate(self, start_block, max_height):
+        if self.owner is None:
+            for block in self.iterate_blocks(start_block, max_height):
+                for bundle, id, length, head, full in self._scan_block(block):
+                    if id == self.first:
+                        self.owner = self.last_metadata.owner
+                        full = full()
+                        data = json.loads(full.data.decode())
+                        self.data_by_offset[data['offset']] = data['txid']
+                        break
+        next_offset = 0
+        for block in self.iterate_blocks(start_block, max_height):
+            for id, length, head, full in self._scan_block(start_block):
+                if next_offset in self.data_by_offset:
+                    next_metadata = self.data_by_offset[next_offset]
+                    next_data = next_metadata['txid']
+                    if next_data] in self.bundle_by_item:
+                        next_bundle = self.bundle_by_item[next_data['txid']]
+                        stream = self.peer.stream(next_txid)
+                        bundle = ANS104BundleHeader.fromstream(stream)
+                        start, end = bundle.get_range(next_data)
+                        stream.seek(start)
+                        data = DataItem.fromstream(stream, end - start)
+                        yield data
+                        del self.data_by_offset[next_offset]
+                        del self.bundle_by_item[next_data]
+                head = head()
+                if head.owner == self.owner:
+                    self.bundle_by_item[full.header.id] = bundle
+                if head.owner == self.owner and length < 50000:
+                    full = full()
+                    try:
+                        data = json.loads(full.data.decode())
+                        if data['first'] != self.first:
+                            continue
+                    except:
+                        continue
+                    self.data_by_offset[data['offset']] = data['txid']
+                    del self.bundle_by_item[full.header.id]
     def block(self, id):
         if type(id) is str:
             Block.frombytes(self.peer.block2_hash(id))
