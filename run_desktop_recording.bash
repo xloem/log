99733281a41fe4f54a5c2b2f735317524ea72cd5
John Doe <johndoe@example.com>
2022-12-04 07:34:14 -0500

    -

--- run_desktop_recording.bash
+++ run_desktop_recording.bash
@@ -1,1 +1,8 @@
-ffmpeg -f x11grab -rtbufsize $((2**31-1)) -i :0 -f pulse -i default -c:v libx264 -preset ultrafast -crf 28 -c:a libopus -f matroska -movflags +faststart - | python3 capture_stdin.py
+#sudo nice -n -5 $(type -p ffmpeg) -f x11grab -rtbufsize $((2**31-1)) -i :0 -f pulse -i default -c:v libx264 -preset ultrafast -crf 28 -c:a libopus -f matroska -movflags +faststart - | python3 capture_stdin.py
+#nice -n 5 $(type -p ffmpeg) -f x11grab -rtbufsize $((2**31-1)) -i :0 -f pulse -i default -c:v libx264 -preset ultrafast -crf 28 -c:a libopus -f matroska -movflags +faststart - | python3 capture_stdin.py
+sudo echo hi
+{
+    ffmpeg -f x11grab -rtbufsize $((2**31-1)) -i :0 -f pulse -i default -c:v libx264 -preset ultrafast -crf 28 -c:a libopus -f matroska -movflags +faststart - &
+    sudo renice -n -5 --pid $! 1>&2
+    wait 1>&2
+}  | python3 capture_stdin.py
---
+++ run_headsupcam.bash
@@ -0,0 +1,5 @@
+while true
+do
+    ffplay -f v4l2 -video_size 320x200  /dev/video1
+done
+
